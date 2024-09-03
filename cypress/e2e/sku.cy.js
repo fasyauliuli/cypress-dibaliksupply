1d1720aee03f00483f6302fd4a8d2e54a675a191
fasyauli01 <aulia.fasya@efishery.com>
2024-09-03 23:26:48 +0700

    dbs create sku

---
+++ cypress/e2e/sku.cy.js
@@ -0,0 +1,136 @@
+require('@cypress/xpath');
+const faker = require('faker');
+import selectors from '../support/pageObjects/selectors.js';
+
+describe('template spec', () => {
+  it('passes', () => {
+    const product = faker.commerce.productName();
+
+    cy.visit('https://stg-dibalik-supply.efishery.com')
+
+    //login
+    cy.get(selectors.username).type('eshrimp.1.atesting@efishery.com')
+    cy.get(selectors.password).type('P@ssw0rd123')
+    cy.xpath(selectors.bt_masuk).click()
+
+    //view list
+    cy.xpath(selectors.dd_sku_management).click()
+    cy.get(selectors.menu_sku).click()
+    cy.xpath(selectors.title_sku_list).should('have.text', 'SKU')
+    cy.get(selectors.bt_create).should('have.text', 'Tambah SKU')
+    cy.get(selectors.field_search).should('be.visible')
+    cy.get(selectors.brand_name).should('be.visible')
+    cy.get(selectors.product_name).should('be.visible')
+    cy.get(selectors.category_name).should('be.visible')
+    cy.get(selectors.bt_filter).should('have.text', 'Filter')
+    cy.get(selectors.bt_reset).should('have.text', 'Reset')
+    cy.xpath(selectors.col_kode_sku).should('have.text', 'Kode SKU')
+    cy.xpath(selectors.col_nama_sku).should('have.text', 'Nama SKU')
+    cy.xpath(selectors.col_kategori).should('have.text', 'Kategori')
+    cy.xpath(selectors.col_status).should('have.text', 'Status')
+    cy.get(selectors.bt_detail).should('have.text', 'Detail')
+
+    //search data
+    cy.get(selectors.field_search).type('kmp')
+    cy.get(selectors.bt_filter).click()
+    cy.get(selectors.list_sku).contains('KMP')
+    cy.get(selectors.bt_reset).click()
+
+    //search combined data
+    cy.get(selectors.field_search).type('kmp')
+    cy.get(selectors.category_name).type('Fish Feed')
+    cy.xpath(selectors.dd_fish_feed).click()
+    cy.get(selectors.bt_filter).click()
+    cy.get(selectors.list_sku).contains('KMP')
+    cy.get(selectors.list_sku).contains('Fish Feed')
+    cy.get(selectors.bt_reset).click()
+
+    //search no data
+    cy.get(selectors.field_search).type('smoke test')
+    cy.get(selectors.bt_filter).click()
+    cy.xpath(selectors.search_no_data).should('have.text', 'No SKU Data')
+
+    //create page
+    cy.get(selectors.bt_create).click()
+    cy.xpath(selectors.title_create_sku).should('have.text', 'Create Sku')
+
+    //e2e create sku
+    cy.get(selectors.brand_name).type('testcobax')
+    cy.xpath(selectors.dd_brand_name).click()
+    cy.get(selectors.dd_category_name).type('vitamin')
+    cy.xpath(selectors.vitamin_category).click()
+    cy.get(selectors.product_name).type(product)
+    cy.get(selectors.dd_uom).type('pack')
+    cy.xpath(selectors.uom_pack).click()
+    cy.get(selectors.dd_commodity).type('nilem')
+    cy.xpath(selectors.commodity_nilem).click()
+    cy.get(selectors.radio_b2b).click()
+    cy.get(selectors.radio_b2c).click()
+    cy.get(selectors.radio_tax).click()
+    cy.get(selectors.weight).type('50')
+    cy.get(selectors.weight_uom).type('gr')
+    cy.xpath(selectors.gr_uom).click()
+    cy.get(selectors.sku_name).should('not.have.value', '')
+    cy.get(selectors.sku_code).should('not.have.value', '')
+
+    // Load a file from fixtures
+    cy.fixture('macaroon.jpeg').then(fileContent => {
+      // Attach the file to the file input
+      cy.get('input[type="file"]').attachFile({
+        fileContent: fileContent,
+        fileName: 'macaroon.jpeg',
+        mimeType: 'image/jpeg'
+      });
+    });
+
+    cy.xpath(selectors.upload_image_success).should('be.visible')
+    cy.get(selectors.bt_register).click()
+    cy.xpath(selectors.bt_confirm).click()
+    cy.xpath(selectors.success_submit_sku).should('be.visible')
+
+    //detail page
+    cy.get(selectors.field_search).type('testcobax')
+    cy.get(selectors.bt_filter).click()
+    cy.get(selectors.bt_detail).click()
+    cy.xpath(selectors.title_detail_sku).should('be.visible')
+    cy.get(selectors.bt_edit).should('be.enabled')
+    cy.get(selectors.detail_sku_name).should('be.visible')
+    cy.get(selectors.detail_sku_code).should('be.visible')
+    cy.get(selectors.status_sku).should('be.visible')
+    cy.get(selectors.detail_category).should('be.visible')
+    cy.get(selectors.detail_packaging).should('be.visible')
+    cy.get(selectors.detail_tax).should('be.visible')
+    cy.get(selectors.detail_weight).should('be.visible')
+    cy.get(selectors.detail_dimension).should('be.visible')
+    cy.get(selectors.detail_nutritions).should('be.visible')
+
+    //edit
+    cy.get(selectors.bt_edit).click()
+    cy.xpath(selectors.title_edit_sku).should('be.visible')
+    cy.get(selectors.brand_name).should('be.disabled')
+    cy.get(selectors.dd_category_name).should('be.disabled')
+    cy.get(selectors.edit_product_name).should('be.disabled')
+    cy.get(selectors.dd_uom).should('be.disabled')
+    cy.get(selectors.dd_commodity).should('be.disabled')
+    cy.get(selectors.radio_b2b).should('be.disabled')
+    cy.get(selectors.radio_b2c).should('be.disabled')
+    cy.get(selectors.edit_weight).should('be.disabled')
+    cy.get(selectors.weight_uom).should('be.disabled')
+    cy.get(selectors.length).type('10')
+    cy.get(selectors.width).type('3')
+    cy.get(selectors.height).type('5')
+    cy.get(selectors.bt_register).click()
+    cy.xpath(selectors.bt_confirm).click()
+    cy.xpath(selectors.title_detail_sku).should('be.visible')
+    cy.wait(2000)
+
+    //deactive sku
+    cy.get(selectors.switch_status_sku).click()
+    cy.get(selectors.input_reason).type('udah ga dijual')
+    cy.get(selectors.submit_status).click()
+    cy.get(selectors.bt_confirm_status).click()
+    cy.xpath(selectors.success_nonaktif_sku).should('be.visible')
+    cy.get(selectors.status_sku).should('have.text', 'Non-aktif')
+    cy.get(selectors.switch_status_sku).should('be.disabled')
+  })
+})
---
+++ cypress/support/pageObjects/selectors.js
@@ -0,0 +1,77 @@
+export default {
+    //login
+    username: '#username',
+    password: '#password',
+    bt_masuk: '//button[.="Masuk"]',
+
+    //list sku
+    dd_sku_management: "//span[.='SKU Management']",
+    menu_sku: '[data-testid="sider-sku-management-sku"]',
+    title_sku_list: "//h3[.='SKU']",
+    bt_create: '[data-testid="btn_create"]',
+    field_search: '[data-testid="sku_list_page-search-input"]',
+    brand_name: '#brand_name',
+    product_name: '#product_name',
+    category_name: '#category_name',
+    bt_filter: '[data-testid="dd_filter_button"]',
+    bt_reset: '[data-testid="dd_reset_filter_button"]',
+    col_kode_sku: "//th[.='Kode SKU']",
+    col_nama_sku: "//th[.='Nama SKU']",
+    col_kategori: "//th[.='Kategori']",
+    col_status: "//th[.='Status']",
+    bt_detail: '[data-testid="btn_show_1"]',
+    list_sku: '[data-testid="sku_list_page-table"]',
+    dd_fish_feed: "(//div[text()='Fish Feed'])[2]",
+    search_no_data: "//div[text()='No SKU Data']",
+
+    //create
+    bt_create: '[data-testid="btn_create"]',
+    title_create_sku: "//h3[.='Create Sku']",
+    dd_brand_name: "//div[text()='testCobax']",
+    dd_category_name: '#product_category_name',
+    vitamin_category: "//div[text()='Vitamin']",
+    dd_uom: '#uom_name',
+    uom_pack: "//div[text()='Pack']",
+    dd_commodity: '#commodity',
+    commodity_nilem: "//div[text()='Nilem']",
+    radio_b2b: '[value="B2B"]',
+    radio_b2c: '[value="B2C"]',
+    radio_tax: '[value="NO TAX"]',
+    weight: '#weight',
+    weight_uom: '#weight_uom',
+    gr_uom: "//div[text()='Gr']",
+    sku_name: '[data-testid="txtfld_sku_name"]',
+    sku_code: '[data-testid="txtfld_sku_code"]',
+    upload_image_success: "//div[@class='ant-upload-list-item ant-upload-list-item-done']",
+    bt_register: '[data-testid="btn_register"]',
+    bt_confirm: "(//button[.='Simpan'])[3]",
+    success_submit_sku: "//div[contains(text(),'SKU berhasil ditambahkan')]",
+
+    //detail
+    title_detail_sku: "//h3[.='Detail SKU']",
+    bt_edit: '[data-testid="btn_edit"]',
+    detail_sku_name: '[data-testid="txt_sku_name"]',
+    detail_sku_code: '[data-testid="txt_sku_code"]',
+    status_sku: '[data-testid="tag-status"]',
+    detail_category: '[data-testid="txt_product_category_complete_name"]',
+    detail_packaging: '[data-testid="txt_packaging_type"]',
+    detail_tax: '[data-testid="txt_tax"]',
+    detail_weight: '[data-testid="txt_weight"]',
+    detail_dimension: '[data-testid="txt_dimension"]',
+    detail_nutritions: '[data-testid="txt_sku_nutritions"]',
+
+    //edit
+    title_edit_sku: "//h3[.='Edit Sku']",
+    edit_product_name: '[data-testid="txtfld_product_name"]',
+    edit_weight: '[data-testid="txtfld_weight"]',
+    length: '[data-testid="txtfld_length"]',
+    width: '[data-testid="txtfld_width"]',
+    height: '[data-testid="txtfld_height"]',
+
+    //deactive
+    switch_status_sku: '[data-testid="switch_sku_active"]',
+    input_reason: '[data-testid="txt_remark"]',
+    submit_status: '[data-testid="btn_sku_status_submit"]',
+    bt_confirm_status: '#btn_sku_status_confirm_submit',
+    success_nonaktif_sku: "//div[contains(text(),'SKU berhasil dinonaktifkan.')]"
+};

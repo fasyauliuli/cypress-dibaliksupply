7a5d086a35366538b2fae53157d721b45a9f882f
fasyauli01 <aulia.fasya@efishery.com>
2024-09-14 17:46:20 +0700

    chore: refactor config

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,21 @@
 const { defineConfig } = require("cypress");
-// const { allureCypress } = require("allure-cypress/reporter");
-// const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      const allureWriter = require('@shelex/cypress-allure-plugin/writer');
       allureWriter(on, config);
       return config;
     },
     testIsolation: false,
     env: {
-    baseUrl: 'https://stg-dibalik-supply.efishery.com',
-    email: 'eshrimp.1.atesting@efishery.com',
-    password: 'P@ssw0rd123'
+      baseUrl: 'https://stg-dibalik-supply.efishery.com',
+      email: 'eshrimp.1.atesting@efishery.com',
+      password: 'P@ssw0rd123'
     },
-    // reporter: 'mocha-allure-reporter',
-    // reporterOptions: {
-    //   resultsDir: 'allure-results', // This is where Allure results will be stored
-    // },
     reporter: 'mocha-allure-reporter',
     reporterOptions: {
-      resultsDir: 'allure-results', // Ensure this matches the directory you expect
+      resultsDir: 'allure-results',
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-file-upload';
-// import "allure-cypress/commands";
 import '@shelex/cypress-allure-plugin';
 
 
